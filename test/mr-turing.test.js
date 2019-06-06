29cdf220e7ceb105bdc3aaeff3494c92ac2b16db
Diego Lopes <diego-chez@hotmail.com>
2019-06-06 09:36:52 -0300

    Creates the bots list test

--- test/mr-turing.test.js
+++ test/mr-turing.test.js
@@ -3,11 +3,19 @@ const getToken = require("../util/getToken");
 const getBotList = require("../util/getBotList");
 const makeQuestion = require("../util/makeQuestion");
 
+const MOCK_TOKEN = "";
+
 const keys = require("../helpers/keys.json");
 
 describe("Mr turing API Access", function() {
   it("Should get an valid access token", async () => {
-    const { token_type } = await getToken(keys);
+    const { token_type, access_token } = await getToken(keys);
+    MOCK_TOKEN = access_token;
     assert.deepEqual(token_type, "Bearer");
+  });
+
+  it("Should bring the bots list", async () => {
+    const { results } = await getBotList(MOCK_TOKEN);
+    assert.ok(results);
   });
 });
