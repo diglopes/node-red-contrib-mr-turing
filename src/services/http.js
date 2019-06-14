20cee52eb2995ed8862c458f273de0705d23fd48
Diego Lopes <diego-chez@hotmail.com>
2019-06-14 11:24:44 -0300

    updates the baseURL

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const http = axios.create({
-  baseURL: "https://qa.misterturing.com/rest"
+  baseURL: "https://app.misterturing.com/rest"
 });
 
 module.exports = http;
