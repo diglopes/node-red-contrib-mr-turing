bf65dcb421ab52721889910a46aefc96a597795f
Diego Lopes <diego-chez@hotmail.com>
2020-04-28 08:58:44 -0300

    Remove not used variables

--- nodes/mr-turing-auth.js
+++ nodes/mr-turing-auth.js
@@ -2,12 +2,7 @@ module.exports = function (RED) {
   function MrTuringAuth(config) {
     RED.nodes.createNode(this, config);
     let node = this;
-
-    // Variables
     node.user = config.user;
-    const clientID = node.credentials.clientID;
-    const clientSecret = node.credentials.clientSecret;
-    const password = node.credentials.password;
   }
 
   RED.nodes.registerType("mr-turing-auth", MrTuringAuth, {
