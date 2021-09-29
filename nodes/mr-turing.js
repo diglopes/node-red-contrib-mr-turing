8dc9d808ce348275f0b3895c235f7142aa06abb9
Diego Lopes <diego-chez@hotmail.com>
2021-09-29 18:05:36 -0300

    feat: load all knowledge bases

--- nodes/mr-turing.js
+++ nodes/mr-turing.js
@@ -7,22 +7,32 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, n);
 
     // Configuration options passed by Node red
+    const node = this
     this.botName = n.botName;
     this.random = n.random || false;
     this.login = RED.nodes.getNode(n.login);
+    this.kbs = []
 
     // Config node state
     this.token = "";
+    const { email } = this.login
+    const { password } = this.login.credentials
+    mrTuringService
+      .login(email, password)
+      .then(({ access_token: accessToken }) => {
+        node.accessToken = accessToken
+      })
+
+    // Editor Knowledge bases endpoint
+    RED.httpNode.get(`/nodes/${n.id}/mr_turing/knowledge_bases`, async (_, res) => {
+      let kbs = await mrTuringService.getKnowledgeBases(node.accessToken)
+      kbs = Array.isArray(kbs) ? kbs : []
+      return res.send({ kbs })
+    })
 
     this.on("input", async (msg, send, done) => {
       this.status({});
-      const { email } = this.login
-      const { password } = this.login.credentials
-
-      const authResponse = await mrTuringService.login(email, password)
-    
-      msg.payload = authResponse
-
+      msg.payload = "OK"
       this.status({})
       send(msg)
       done()
