c6c3d631fd8623fbef8cb64e575e2e839c9d0030
Diego Lopes <diego-chez@hotmail.com>
2021-10-06 18:48:45 -0300

    feat: keep token in cache for a day

--- nodes/mr-turing.js
+++ nodes/mr-turing.js
@@ -1,28 +1,28 @@
 module.exports = function (RED) {
   "use strict";
+  const NodeCache = require("node-cache")
   const createMrTuringService = require("../lib/mr-turing-service")
   const createStatusNotifier = require("../lib/status-notifier")
-  const mrTuringService = createMrTuringService()
+  const cache = new NodeCache()
+  const constants = require("../lib/constants")
   
   function MrTuring(n) {
     const status = createStatusNotifier(this)
     
     RED.nodes.createNode(this, n);
-
+    
     // Configuration options passed by Node red
     const node = this
     this.botName = n.botName;
     this.random = n.random || false;
     this.login = RED.nodes.getNode(n.login);
     this.knowledgeBase = n.knowledgeBase
-
-    // Authenticate user
-    status.login()
+    
+    // // Authenticate user
     const { email } = this.login
     const { password } = this.login.credentials
-    mrTuringService.login(email, password)
-    status.clear()
-
+    const mrTuringService = createMrTuringService(cache, status, { email, password })
+    
     // Editor Knowledge bases endpoint
     RED.httpNode.get(`/nodes/${n.id}/mr_turing/knowledge_bases`, async (_, res) => {
       let kbs = await mrTuringService.getKnowledgeBases()
@@ -33,12 +33,17 @@ module.exports = function (RED) {
     this.on("input", async (msg, send, done) => {
       status.clear()
       const question = msg.payload
-      status.questioning()
-      const answers = await mrTuringService.ask(question, this.knowledgeBase)
-      msg.payload = answers
-      status.clear()
-      send(msg)
-      done()
+      try {
+        const answers = await mrTuringService.ask(question, this.knowledgeBase)
+        msg.payload = answers
+        status.clear()
+        send(msg)
+        done()
+      } catch (error) {
+        status.error()
+        cache.del(constants.cache.TOKEN_KEY)
+        done(error)
+      }
     });
   }
   RED.nodes.registerType("mr-turing", MrTuring);
